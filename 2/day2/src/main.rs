861353064f3e6a1f6fa4234300f456e021be1c08
Henry Lourtie <henry.lourtie@gmail.com>
2023-12-02 21:24:30 +0100

    made a separate function for the parsing to make it look cleaner

--- 2/day2/src/main.rs
+++ 2/day2/src/main.rs
@@ -4,7 +4,7 @@ use std::path::Path;
 use std::fs::File;
 use std::io::BufReader;
 
-fn is_possible(input:&String) -> bool {
+fn parse_input_into_2d_vec(input: &String) -> Vec<Vec<u32>> {
     let initial_split : Vec<&str> = input.split(":").collect();
     let result;
     if let Some(second_part) = initial_split.get(1) {
@@ -44,6 +44,11 @@ fn is_possible(input:&String) -> bool {
         }
         final_vect.push(rgb_values);
     }
+    final_vect
+}
+
+fn is_possible(input:&String) -> bool {
+    let final_vect = parse_input_into_2d_vec(input);
     println!("{:?}", final_vect);
     let bool_vec: Vec<_> = final_vect.into_iter().map(|x| {
         if x[0]> 12 || x[1]> 13 || x[2]>14 {
@@ -65,45 +70,7 @@ fn is_possible(input:&String) -> bool {
 }
 
 fn cube_of_line_possible (input: &String) -> u32 {
-    let initial_split : Vec<&str> = input.split(":").collect();
-    let result;
-    if let Some(second_part) = initial_split.get(1) {
-        result = second_part.trim();
-    } else {
-        println!("No substring found after ':'");
-        result="";
-    }
-    let second_split :Vec<&str> = result.split(";").collect();
-
-    let mut final_vect : Vec<Vec<u32>> = Vec::new();
-
-    for sect in second_split {
-
-        let mut rgb_values : Vec<u32> = vec![0;3];
-        let colors_combo: Vec<&str> = sect.split(",").collect();
-
-        for color_line in colors_combo{
-
-            let tokens: Vec<&str> = color_line.trim().split_whitespace().collect();
-
-            if let Some(number_of_stones) = tokens.get(0) {
-
-                if let Some(color) = tokens.get(1){
-
-                    if let Ok(count) = number_of_stones.parse::<u32>() {
-
-                        match color {
-                            &"red" => rgb_values[0] += count,
-                            &"green" => rgb_values[1] += count,
-                            &"blue" => rgb_values[2] += count,
-                            _ => (),
-                        }
-                    }
-                }
-            }
-        }
-        final_vect.push(rgb_values);
-    }
+    let final_vect = parse_input_into_2d_vec(input);
     let mut max_values: Vec<u32> = Vec::new();
 
     for col in 0..final_vect[0].len() {
